f68a69dc7bef2ef8aaf3fb8da087bb7dfb8b26bb
Jack Noble <email": null>
2016-09-08 16:17:42 -0500

    Hide API key

--- public/js/application.js
+++ public/js/application.js
@@ -2,8 +2,8 @@ var map;
 function initMap() {
 // Constructor creates a new map - only center and zoom are required.
 map = new google.maps.Map(document.getElementById('map'), {
-  center: {lat: 40.7413549, lng: -73.9980244},
-  zoom: 13
+  center: {lat: 40.7413549, lng: -95.9980244 },
+  zoom: 11
 });
 }
 
