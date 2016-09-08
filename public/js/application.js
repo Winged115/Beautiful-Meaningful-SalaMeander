b8cea81d70f84eb75b78f96f9a1a39b684188cd8
Jack Noble <email": null>
2016-09-08 15:04:27 -0500

    Put map on screen

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,9 @@
-$(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
-
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+var map;
+function initMap() {
+// Constructor creates a new map - only center and zoom are required.
+map = new google.maps.Map(document.getElementById('map'), {
+  center: {lat: 40.7413549, lng: -73.9980244},
+  zoom: 13
 });
+}
+
